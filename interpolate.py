8db1138c1ddccf16d063a0c8c3930e9d6dba60ed
Rishabh Vakil <106563505+rivak7@users.noreply.github.com>
2025-07-02 10:40:10 -0700

    Update interpolate.py
    clean up code

--- interpolate.py
+++ interpolate.py
@@ -1,3 +1,5 @@
+# Used to interpolate a set of points from https://www.thrustcurve.org/ to get thrust values every dt
+
 import csv
 
 dt = 0.001
@@ -10,14 +12,13 @@ def load_data(file, column):
             try:
                 data.append((float(row[0]), float(row[column])))
             except ValueError:
-                continue  # Skip headers
+                continue
     return data
 
 s_sensor = load_data("Real.csv", 1)
 v_sensor = load_data("Real.csv", 2)
 a_sensor = load_data("Real.csv", 3)
 
-# Get the maximum time from the first column of the sensor data
 t_max = max(s_sensor, key=lambda x: x[0])[0]
 
 def interpolate_data(column, dt, t_max):
@@ -25,14 +26,11 @@ def interpolate_data(column, dt, t_max):
     t = 0
     i = 0
     while t <= t_max:
-        # Find the two points to interpolate between
         while i < len(column) - 1 and column[i + 1][0] < t:
             i += 1
         if i == len(column) - 1:
-            # If we are at the end of the dataset, append the last value
             interpolated.append(column[i][1])
         else:
-            # Interpolate between column[i] and column[i + 1]
             t1, T1 = column[i]
             t2, T2 = column[i + 1]
             interpolated.append(((t - t1) / (t2 - t1)) * (T2 - T1) + T1)
@@ -54,5 +52,4 @@ def save_interpolated_data(file, t_max, dt, s, v, a):
             writer.writerow([round(t, 3), s[i], v[i], a[i]])
             t += dt
 
-# Save to a CSV file
 save_interpolated_data("interpolateddata.csv", t_max, dt, s, v, a)
